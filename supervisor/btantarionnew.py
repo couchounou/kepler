35aa25f15875389e1cc0d958129eb6d800691285
couchounou <couchounou@hotmail.com>
2026-01-19 11:37:58 +0100

    Enhance notification logging in get_solar_reg_data function to include turn status

--- supervisor/btantarionnew.py
+++ supervisor/btantarionnew.py
@@ -139,7 +139,7 @@ async def get_solar_reg_data(cycles=1):
     async def souscription_notifications(client):
         for handle in [0x000e]:
             try:
-                print(f"[BT SOLAR]   Try start notify -> {handle}")
+                print(f"[BT SOLAR]   Try (turn {turn}) start notify -> {handle}")
                 await asyncio.wait_for(
                     client.start_notify(
                         handle,
