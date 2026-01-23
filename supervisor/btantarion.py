1f15884a29764400796bd19af236620a094c04a6
couchounou <couchounou@hotmail.com>
2026-01-23 23:14:57 +0100

    Add timeout to subprocess call in Bluetooth restart process

--- supervisor/btantarion.py
+++ supervisor/btantarion.py
@@ -51,7 +51,7 @@ class btantarion:
                 if cmd is None:  # Sleep step
                     time.sleep(2)
                 else:
-                    result = subprocess.run(cmd, check=True, capture_output=False, text=True)
+                    result = subprocess.run(cmd, check=True, capture_output=False, text=True,timeout=3)
                     if result.stdout:
                         logging.info(f"    {result.stdout.strip()}")
 
